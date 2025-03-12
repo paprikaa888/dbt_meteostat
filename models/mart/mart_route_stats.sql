d17db7b5883ab4461efae2b2f54c1dc05ae0ad3c
paprikaa888 <73936322+paprikaa888@users.noreply.github.com>
2025-03-12 11:48:45 +0100

    mart route added

---
+++ models/mart/mart_route_stats.sql
@@ -0,0 +1,29 @@
+with flight_route_stats as (
+				SELECT
+					origin
+					,dest
+					,count (flight_number)
+					,count (distinct tail_number) as unique_tails
+					,count (distinct airline) as unique_airlines
+					,avg(actual_elapsed_time_interval) as avg_actual_elapsed_
+					,avg(arr_delay_interval) as avg_arr_delay
+					,avg(dep_delay_interval) as avg_dep_delay
+					,max(arr_delay_interval) as max_arr_delay
+					,min(arr_delay_interval) as min_arr_delay
+					,sum(cancelled) as total_cancelled
+					,sum(diverted) as total_diverted
+				from {{ref('prep_flights')}}
+				group by (origin, dest)
+)
+select o.city as origin_city, 
+		d.city as dest_city,
+		o.name as origin_name,
+		d.name as dest_name,
+		o.country as origin_country,
+		d.country as dest_country,
+		f.*
+from flight_route_stats f
+left join  {{ref('prep_airports')}} o
+	on f.origin=o.faa
+left join  {{ref('prep_airports')}} d
+	on f.dest=d.faa
